d5f5544cb6a9a910854c49b6be5e5663db369935
Jefferson G.T <jeferguto1@outlook.com>
2023-06-26 17:18:56 -0500

    main

--- js/menu.js
+++ js/menu.js
@@ -1,34 +1,37 @@
 const burguerIcon = document.querySelector('.nav__burguer');
 const navOverlay = document.querySelector('.nav__overlay');
 
-burguerIcon.addEventListener('click', ()=>{
-    burguerIcon.classList.toggle('nav__burguer--open');
+burguerIcon.addEventListener("click", () => {
+  burguerIcon.classList.toggle("nav__burguer--open");
 
-    navOverlay.classList.toggle('nav__overlay--view')
+  navOverlay.classList.toggle("nav__overlay--show");
 });
 
-navOverlay.addEventListener('click', (e)=>{
-    e.preventDefault();
-    const currentElement = e.target;
-    //console.log(e.target.classList.value);
+navOverlay.addEventListener("click", (e) => {
+  e.preventDefault();
+  const currentElement = e.target;
+  //console.log(e.target.classList.value);
 
-    if(isActive(currentElement, 'nav__parent')){
-        //console.log('Existe submmenu')
-        const subMenu = currentElement.parentElement.children[1];
-        console.log(subMenu);
+  if ( isActive(currentElement, 'nav__parent')){
+    
+    const subMenu = currentElement.parentElement.children[1];
+    console.log(subMenu);
 
-        if(window.innerWidth < 768){
+    if(window.innerWidth < 700){
 
-            let height = (subMenu.clientHeight == 0)
-                ? subMenu.scrollHeight : 0;
-            
-            subMenu.style.height = `${height}px`;
-        }else{
+      let height = subMenu.clientHeight == 0
+      ? subMenu.scrollHeight: 0;
+      //console.log(subMenu.clientHeight);
 
-        }
+      subMenu.style.height = `${height}px`; 
+
+    }else{
+
     }
+  }
+
 });
 
-function isActive(element, string) {
-    return element.classList.value.includes(String);
+function isActive(element, string){
+  return element.classList.value.includes(string);
 }
